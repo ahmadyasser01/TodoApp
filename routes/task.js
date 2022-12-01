572bacc1f31a84f6cc9f9ba1504ca33de6d464bb
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-12-01 19:44:19 +0200

    feat:protect all tasks endpoints by auth middleware

--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import { protect } from '../controllers/auth.js';
 import { createTask, deleteTask, getTask, updateTask } from '../controllers/task.js';
 
-
 const router = express.Router();
-
+router.use(protect)
 router.route('/')
 .post(createTask)
 
