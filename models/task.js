467a64c9e6a1bb5b0c4a4405daa99f8539209161
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-12-01 18:02:31 +0200

    task model

---
+++ models/task.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,"Task title is required"]
+    },
+    description:{
+        type:String,
+        required:[true,"Task description is required"]
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+});
+
+const Task = mongoose.model('Task',taskSchema);
+export default Task;
